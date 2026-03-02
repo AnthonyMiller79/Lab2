b5aaa4a8dafc23a9d8b6a102ff959776205bcf4a
AnthonyMiller79 <24milleranthony@gmail.com>
2026-03-01 19:51:45 -0700

    finished

---
+++ src/sevenseg_decoder_tb.vhd
@@ -0,0 +1,104 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/01/2026 04:08:16 PM
+-- Design Name: 
+-- Module Name: sevenseg_decoder_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+entity sevenseg_decoder_tb is
+end sevenseg_decoder_tb;
+
+architecture Behavioral of sevenseg_decoder_tb is
+    component sevenseg_decoder is
+    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0)
+       );
+     end component;
+     signal w_hex : std_logic_vector (3 downto 0):= (others=> '0');
+    signal w_seg : std_logic_vector (6 downto 0);
+begin
+    sevenseg_decoder_t :sevenseg_decoder
+    port map(
+    i_Hex => w_hex,
+    o_seg_n => w_seg
+    );
+    test_process : process
+    begin
+    w_hex <= x"0"; wait for 10 ns;
+    assert w_seg = "1000000" report "error on 0" severity failure;
+    
+    w_hex <= x"1"; wait for 10 ns;
+    assert w_seg = "1111001" report "error on 1" severity failure;
+    
+    w_hex <= x"2"; wait for 10 ns;
+    assert w_seg = "0100100" report "error on 2" severity failure;
+    
+    w_hex <= x"3"; wait for 10 ns;
+    assert w_seg = "0110000" report "error on 3" severity failure;
+    
+    w_hex <= x"4"; wait for 10 ns;
+    assert w_seg = "0011001" report "error on 4" severity failure;
+   
+    w_hex <= x"5"; wait for 10 ns;
+    assert w_seg = "0010010" report "error on 5" severity failure;
+    
+    w_hex <= x"6"; wait for 10 ns;
+    assert w_seg = "0000010" report "error on 6" severity failure;
+    
+    w_hex <= x"7"; wait for 10 ns;
+    assert w_seg = "1111000" report "error on 7" severity failure;
+    
+    w_hex <= x"8"; wait for 10 ns;
+    assert w_seg = "0000000" report "error on 8" severity failure;
+    
+    w_hex <= x"9"; wait for 10 ns;
+    assert w_seg = "0010000" report "error on 9" severity failure;
+    
+    w_hex <= x"A"; wait for 10 ns;
+    assert w_seg = "0001000" report "error on A" severity failure;
+    
+    w_hex <= x"B"; wait for 10 ns;
+    assert w_seg = "0000011" report "error on B" severity failure;
+    
+    w_hex <= x"C"; wait for 10 ns;
+    assert w_seg = "1000110" report "error on C" severity failure;
+    
+    w_hex <= x"D"; wait for 10 ns;
+    assert w_seg = "0100001" report "error on D" severity failure;
+    
+    w_hex <= x"E"; wait for 10 ns;
+    assert w_seg = "0000110" report "error on E" severity failure;
+    
+    w_hex <= x"F"; wait for 10 ns;
+    assert w_seg = "0001110" report "error on F" severity failure;
+
+    wait;
+	end process;	
+	-----------------------------------------------------	
+	
+end Behavioral;
